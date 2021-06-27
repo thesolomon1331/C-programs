85a1d471821728df2ba1e75e16b3a10a72e1251d
Solomon Raju G <53774710+thesolomon1331@users.noreply.github.com>
2021-06-27 22:58:07 +0530

    Create Addtion of two numbers.c
    Program to add two numbers in C

---
+++ Addtion of two numbers.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+  int a,b;
+  printf("Enter two numbers: ");
+  scanf("%d %d",&a,&b);
+  printf("Addtion of two numbers %d and %d is %d : ",a,b,a+b);
+  return 0;
+}
