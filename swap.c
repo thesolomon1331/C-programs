8df1b6a670794b0dfadb970e00c117dc7ed0bc9e
Solomon Raju G <53774710+thesolomon1331@users.noreply.github.com>
2022-04-06 17:38:43 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,5 +1,7 @@
+/*Swapping Using Temp Variable */
 #include<stdio.h>
-int main() {
+int main() 
+{
       double a,b,temp;
       printf("Enter first number: ");
       scanf("%lf", &a);
@@ -15,3 +17,21 @@ int main() {
       return 0;
 }
 
+/* Swaping with out using Temp Variable */
+#include<stdio.h>
+int main() 
+{
+      double a,b;
+      printf("Enter first number: ");
+      scanf("%lf", &a);
+      printf("Enter second number: ");
+      scanf("%lf", &b);
+      
+      a=a+b;
+      b=a-b;
+      a=a-b;
+      
+      printf("\nAfter swapping, firstNumber = %.2lf\n", a);
+      printf("After swapping, secondNumber = %.2lf", b);
+      return 0;
+}
